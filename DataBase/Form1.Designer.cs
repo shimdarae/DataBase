2b472f37c9ac63af7369cfb8aeede2e469445d45
shimdarae <masupermanxd@gmail.com>
2016-05-25 11:20:28 +0300

    programme add

---
+++ DataBase/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace DataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.createDataBase = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.addTable = new System.Windows.Forms.Button();
+            this.deleteTable = new System.Windows.Forms.Button();
+            this.deleteColumn = new System.Windows.Forms.Button();
+            this.addColumn = new System.Windows.Forms.Button();
+            this.saveChanges = new System.Windows.Forms.Button();
+            this.deleteRow = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.DBName = new System.Windows.Forms.TextBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Location = new System.Drawing.Point(204, 22);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(64, 13);
+            this.title.TabIndex = 60;
+            this.title.Text = "Заголовок:";
+            // 
+            // createDataBase
+            // 
+            this.createDataBase.Location = new System.Drawing.Point(87, 99);
+            this.createDataBase.Name = "createDataBase";
+            this.createDataBase.Size = new System.Drawing.Size(123, 23);
+            this.createDataBase.TabIndex = 59;
+            this.createDataBase.Text = "Создать";
+            this.createDataBase.UseVisualStyleBackColor = true;
+            this.createDataBase.Click += new System.EventHandler(this.createDataBase_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(274, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 20);
+            this.textBox2.TabIndex = 58;
+            // 
+            // addTable
+            // 
+            this.addTable.Enabled = false;
+            this.addTable.Location = new System.Drawing.Point(327, 58);
+            this.addTable.Name = "addTable";
+            this.addTable.Size = new System.Drawing.Size(142, 23);
+            this.addTable.TabIndex = 57;
+            this.addTable.Text = "Добавить таблицу";
+            this.addTable.UseVisualStyleBackColor = true;
+            this.addTable.Click += new System.EventHandler(this.addTable_Click);
+            // 
+            // deleteTable
+            // 
+            this.deleteTable.Enabled = false;
+            this.deleteTable.Location = new System.Drawing.Point(9, 58);
+            this.deleteTable.Name = "deleteTable";
+            this.deleteTable.Size = new System.Drawing.Size(105, 23);
+            this.deleteTable.TabIndex = 56;
+            this.deleteTable.Text = "Удалить таблицу";
+            this.deleteTable.UseVisualStyleBackColor = true;
+            this.deleteTable.Click += new System.EventHandler(this.deleteTable_Click);
+            // 
+            // deleteColumn
+            // 
+            this.deleteColumn.Enabled = false;
+            this.deleteColumn.Location = new System.Drawing.Point(9, 87);
+            this.deleteColumn.Name = "deleteColumn";
+            this.deleteColumn.Size = new System.Drawing.Size(105, 23);
+            this.deleteColumn.TabIndex = 55;
+            this.deleteColumn.Text = "Удалить колонку";
+            this.deleteColumn.UseVisualStyleBackColor = true;
+            this.deleteColumn.Click += new System.EventHandler(this.deleteColumn_Click);
+            // 
+            // addColumn
+            // 
+            this.addColumn.Enabled = false;
+            this.addColumn.Location = new System.Drawing.Point(179, 58);
+            this.addColumn.Name = "addColumn";
+            this.addColumn.Size = new System.Drawing.Size(142, 23);
+            this.addColumn.TabIndex = 54;
+            this.addColumn.Text = "Добавить колонку";
+            this.addColumn.UseVisualStyleBackColor = true;
+            this.addColumn.Click += new System.EventHandler(this.addColumn_Click);
+            // 
+            // saveChanges
+            // 
+            this.saveChanges.Enabled = false;
+            this.saveChanges.Location = new System.Drawing.Point(327, 87);
+            this.saveChanges.Name = "saveChanges";
+            this.saveChanges.Size = new System.Drawing.Size(142, 23);
+            this.saveChanges.TabIndex = 53;
+            this.saveChanges.Text = "Сохранить изменения";
+            this.saveChanges.UseVisualStyleBackColor = true;
+            this.saveChanges.Click += new System.EventHandler(this.saveChanges_Click);
+            // 
+            // deleteRow
+            // 
+            this.deleteRow.Enabled = false;
+            this.deleteRow.Location = new System.Drawing.Point(9, 29);
+            this.deleteRow.Name = "deleteRow";
+            this.deleteRow.Size = new System.Drawing.Size(105, 23);
+            this.deleteRow.TabIndex = 52;
+            this.deleteRow.Text = "Удалить строку";
+            this.deleteRow.UseVisualStyleBackColor = true;
+            this.deleteRow.Click += new System.EventHandler(this.deleteRow_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(15, 31);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(502, 244);
+            this.tabControl1.TabIndex = 51;
+            // 
+            // DBName
+            // 
+            this.DBName.Location = new System.Drawing.Point(9, 44);
+            this.DBName.Name = "DBName";
+            this.DBName.Size = new System.Drawing.Size(207, 20);
+            this.DBName.TabIndex = 49;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(87, 70);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(123, 23);
+            this.openButton.TabIndex = 48;
+            this.openButton.Text = "Открыть";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.DBName);
+            this.groupBox1.Controls.Add(this.openButton);
+            this.groupBox1.Controls.Add(this.createDataBase);
+            this.groupBox1.Location = new System.Drawing.Point(535, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 207);
+            this.groupBox1.TabIndex = 66;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "БД:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 60;
+            this.label1.Text = "Имя:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.deleteRow);
+            this.groupBox2.Controls.Add(this.title);
+            this.groupBox2.Controls.Add(this.deleteTable);
+            this.groupBox2.Controls.Add(this.saveChanges);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.addColumn);
+            this.groupBox2.Controls.Add(this.deleteColumn);
+            this.groupBox2.Controls.Add(this.addTable);
+            this.groupBox2.Location = new System.Drawing.Point(21, 293);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(483, 133);
+            this.groupBox2.TabIndex = 67;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Элементы управления:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 453);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "База данных";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button createDataBase;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button addTable;
+        private System.Windows.Forms.Button deleteTable;
+        private System.Windows.Forms.Button deleteColumn;
+        private System.Windows.Forms.Button addColumn;
+        private System.Windows.Forms.Button saveChanges;
+        private System.Windows.Forms.Button deleteRow;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TextBox DBName;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DataBase/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DataBase
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog folderBrowserDialog = null;
+        DialogResult dialogResult;
+        DataSet dataBase;
+        TextReader reader;
+        TabPage tabPage;
+        DataGridView data;
+        List<DataGridView> tables;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            tables = new List<DataGridView>();
+        
+    }
+
+
+        public void SelectTheDatabase()
+        {
+            this.folderBrowserDialog = new FolderBrowserDialog();
+            this.dialogResult = this.folderBrowserDialog.ShowDialog();
+            this.DBName.Text = this.folderBrowserDialog.SelectedPath;
+        }
+
+        public void ReadSchemas()
+        {
+            foreach (DataTable table in this.dataBase.Tables)
+            {
+                CreateColums(table);
+
+                InputData(table);
+            }
+        }
+
+
+        public void ReadAllTables()
+        {
+            this.reader = new StreamReader(this.folderBrowserDialog.SelectedPath + @"\main.txt");
+
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                this.dataBase.Tables.Add(line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+        public void CreateColums(DataTable table)
+        {
+            this.reader = new StreamReader(this.folderBrowserDialog.SelectedPath + @"\" + table.TableName + @"\main.txt");
+            string column = reader.ReadLine();
+            while (column != null)
+            {
+                table.Columns.Add(column);
+                column = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+        public void InputData(DataTable table)
+        {
+            this.reader = new StreamReader(this.folderBrowserDialog.SelectedPath + @"\" + table.TableName + @"\data.txt");
+            string data = reader.ReadLine();
+            while (data != null)
+            {
+                table.Rows.Add(data.Split(' '));
+                data = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+
+        public DataGridView CreateDataGridView()
+        {
+            data = new DataGridView();
+            data.Width = 700;
+            data.Height = 300;
+            return data;
+        }
+
+        public void ShowData()
+        {
+            foreach (DataTable table in this.dataBase.Tables)
+            {
+                tabPage = new TabPage(table.TableName);
+                tabControl1.TabPages.Add(tabPage);
+                data = CreateDataGridView();
+                data.DataSource = table;
+
+                tabPage.Controls.Add(data);
+                tables.Add(data);
+            }
+        }
+        
+
+        
+        public int CurrentTabPageIndex()
+        {
+            tabPage = tabControl1.SelectedTab;
+            bool find = false;
+            int i = 0;
+            while (!find && i < tabControl1.TabCount)
+            {
+                if (tabControl1.TabPages[i] == tabPage)
+                {
+                    find = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+
+        private void deleteRow_Click(object sender, EventArgs e)
+        {
+            int i = CurrentTabPageIndex();
+            int j = tables[i].SelectedCells[0].RowIndex;
+            dataBase.Tables[i].Rows.RemoveAt(j);
+        }
+
+        private void deleteColumn_Click(object sender, EventArgs e)
+        {
+            int i = CurrentTabPageIndex();
+            int j = tables[i].SelectedCells[0].ColumnIndex;
+            dataBase.Tables[i].Columns.RemoveAt(j);
+        }
+
+        private void deleteTable_Click(object sender, EventArgs e)
+        {
+            int i = CurrentTabPageIndex();
+            dataBase.Tables.RemoveAt(i);
+            tabControl1.TabPages.RemoveAt(i);
+            tables.RemoveAt(i);
+        }
+
+        private void addColumn_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                int i = CurrentTabPageIndex();
+                dataBase.Tables[i].Columns.Add(textBox2.Text);
+                textBox2.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Введите имя!");
+            }
+        }
+
+        private void addTable_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                dataBase.Tables.Add(textBox2.Text);
+                tabPage = new TabPage(textBox2.Text);
+                tabControl1.TabPages.Add(tabPage);
+                data = CreateDataGridView();
+                data.DataSource = dataBase.Tables[dataBase.Tables.Count - 1];
+                tabPage.Controls.Add(data);
+                tables.Add(data);
+                textBox2.Clear();
+                tabControl1.SelectedIndex = dataBase.Tables.Count - 1;
+            }
+            else
+            {
+                MessageBox.Show("Введите имя!");
+            }
+        }
+
+        private void saveChanges_Click(object sender, EventArgs e)
+        {
+            StreamWriter swMain = new StreamWriter(folderBrowserDialog.SelectedPath + @"\main.txt");
+            StreamWriter sw;
+            StreamWriter tablewrmain;
+            
+
+            foreach (DataTable table in dataBase.Tables)
+            {
+                swMain.WriteLine(table.TableName);
+                Directory.CreateDirectory(this.folderBrowserDialog.SelectedPath + @"\" + table.TableName);
+
+                sw = new StreamWriter(this.folderBrowserDialog.SelectedPath + @"\" + table.TableName + @"\data.txt");
+                tablewrmain = new StreamWriter(this.folderBrowserDialog.SelectedPath + @"\" + table.TableName + @"\main.txt");
+
+                int countColums = table.Columns.Count;
+                int countR = table.Rows.Count;
+
+                for (int i = 0; i < countColums; i++)
+                {
+                    tablewrmain.WriteLine(table.Columns[i].ColumnName);
+                }
+                tablewrmain.Close();
+
+                for (int i = 0; i < countR; i++)
+                {
+                    string line = "";
+                    for (int j = 0; j < countColums - 1; j++)
+                    {
+                        line += table.Rows[i][j].ToString() + " ";
+                    }
+                    line += table.Rows[i][countColums - 1].ToString();
+                    sw.WriteLine(line);
+                }
+                sw.Close();
+            }
+            swMain.Close();
+            MessageBox.Show("Сохранено!");
+        }
+
+        
+
+        private void createDataBase_Click(object sender, EventArgs e)
+        {
+            saveChanges.Enabled = true;
+            addTable.Enabled = true;
+            addColumn.Enabled = true;
+            deleteColumn.Enabled = true;
+            deleteRow.Enabled = true;
+            deleteTable.Enabled = true;
+            dataBase = new DataSet();
+            SelectTheDatabase();
+            MessageBox.Show("Создано!");
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            saveChanges.Enabled = true;
+            addTable.Enabled = true;
+            addColumn.Enabled = true;
+            deleteColumn.Enabled = true;
+            deleteRow.Enabled = true;
+            deleteTable.Enabled = true;
+
+            this.dataBase = new DataSet();
+
+            SelectTheDatabase();
+
+            ReadAllTables();
+
+            ReadSchemas();
+
+            ShowData();
+        }
+    }
+    
+}
